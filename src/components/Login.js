e211af70f5aaaa61d7ff8d62c21a87b6e55fa9d2
María Beltrán <mariabeltra.98@gmail.com>
2020-03-10 17:26:48 -0300

    view login

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,14 +5,16 @@ import '../App.css';
 const Login = ()=> {
   return (
     <div className="login_container">
+        <h3>inicio de sesión</h3>
       <div className='container_email'>
           
-          <input type='email' placeholder='email'/>
+          <input className='inputs' type='email' placeholder='email'/>
       </div>
       <div className='container_password'>
           
-          <input type='password' placeholder='contraseña'/>
+          <input className='inputs' type='password' placeholder='contraseña'/>
       </div>
+      <button>Iniciar</button>
     </div>
   );
 }
