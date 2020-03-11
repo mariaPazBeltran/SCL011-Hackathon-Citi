351e18f693f3ec64e231c217cf78cfd610eb5690
María Beltrán <mariabeltra.98@gmail.com>
2020-03-11 16:19:12 -0300

    pinPad

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
-
+import {BrowserRouter as Router,
+  Route, Redirect} from 'react-router-dom';
 import './App.css';
 import Login from './components/Login';
+import { auth } from 'firebase';
+import Pin from './components/pin/Pin';
 
 function App() {
   return (
     <div className="App">
-      <Login/>
+      <Router>
+    <Route exact path="/">
+  {auth ? <Redirect to="/Pin" /> : <Login/>}
+</Route>
+        
+        <Route path="/pin" component={ Pin } />
+        
+    </Router>
     </div>
   );
 }
