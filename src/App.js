8fe91128c1297988da67eb3352953b8df0e24240
María Paz Beltrán <53879933+mariaPazBeltran@users.noreply.github.com>
2020-03-11 16:25:42 -0300

    Feature/login (#2)
    * view login
    
    * pinPad

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
-
+import {BrowserRouter as Router,
+  Route, Redirect} from 'react-router-dom';
 import './App.css';
 import Login from './components/Login';
+import { auth } from 'firebase';
+import Pin from './components/pin/Pin';
 import CardInfo from './components/Card';
 import ShowCard from './components/showCards';
 
 function App() {
   return (
     <div className="App">
-      <Login/>
+      <Router>
+    <Route exact path="/">
+  {auth ? <Redirect to="/Pin" /> : <Login/>}
+</Route>
+        
+        <Route path="/pin" component={ Pin } />
+        
+    </Router>
     </div>
   );
 }
